d31bc9e17f633b44f36b2d391239e25b794585ee
ourskillstore033 <ourskillstore@gmail.com>
2026-03-26 16:09:34 +0530

    Add files via upload

---
+++ package.js
@@ -0,0 +1,13 @@
+{
+  "name": "cointrx-bot",
+  "version": "1.0.0",
+  "main": "bot.js",
+  "scripts": {
+    "start": "node bot.js"
+  },
+  "dependencies": {
+    "node-telegram-bot-api": "^0.61.0",
+    "node-schedule": "^2.1.1",
+    "mongoose": "^7.0.0"
+  }
+}
